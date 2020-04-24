daae0721824ad4ef2825c7ad43e5a1db7bd0b431
LCBHSStudent <swordland288@gmail.com>
2020-04-25 00:25:14 +0800

    first created this repo

---
+++ res/shader/compile.bat
@@ -0,0 +1,4 @@
+A:\Depending\Vulkan\1.2.131.2\Bin32\glslangValidator.exe -V .\sourceShader\shader.frag
+A:\Depending\Vulkan\1.2.131.2\Bin32\glslangValidator.exe -V .\sourceShader\shader.vert
+move frag.spv .\compiledShader\
+move vert.spv .\compiledShader\
